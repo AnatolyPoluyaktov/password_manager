0cca6e13875ea1cfc516699c72fe5d8c63deade7
Anatoly Poluyaktov <avpoluyaktov@gmail.com>
2020-07-22 04:17:52 +0300

    written path of loading and writing data

--- store.py
+++ store.py
@@ -1,15 +1,50 @@
 import os
-path = "/home/blackandromeda/PycharmProjects/password_manager/"
+import json
+with open("path","r") as fd:
+    path = fd.read()
 class Borg:
     _shared_state = {}
     def __init__(self):
         self.__dict__ = self._shared_state
 
-class Singleton(Borg):
+class Storage(Borg):
     def __init__(self):
         Borg.__init__(self)
-        if os.path.exists(path):
-            pass
+        self.d = {}
+
+    def read(self):
+        if os.path.isfile(path):
+            if os.stat(path).st_size == 0:
+                self.d = {}
+            else:
+                with open(path,"r") as fd:
+                    self.d = json.load(fd)
+        else:
+            with open(path,"w") as fd:
+                self.d = {}
+    def write(self):
+        with open(path,"w") as fd:
+            json.dump(self.d,fd)
+    def put(self,domain,login,password):
+        if domain in self.d:
+            self.d[domain].append((login,password))
+        else:
+            self.d[domain] = []
+            self.d[domain].append((login, password))
+    def get_data(self,domain):
+        return self.d[domain]
 
 
+if __name__ == "__main__":
+    data = Storage()
+    data.read()
+
+
+
+
+
+
+
+
+kek = Storage()
 
